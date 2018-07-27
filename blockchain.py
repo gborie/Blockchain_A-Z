3b3bc3f0b949bd2d73103fc3531feb36a491eb69
gborie <gborie7@gmail.com>
2018-07-27 12:06:14 +0100

    Added create_block method

--- blockchain.py
+++ blockchain.py
@@ -19,7 +19,21 @@ class Blockchain:
     def __init__(self):
         # Initialize the chain containing the blocks as an empty list
         self.chain = []
-        # Create genesis block calling the function create_block that takes 2 arguments:
+        # Create genesis block calling the method create_block that takes 2 arguments:
         ## proof with arbitrary value of 1
         ## previous_hash with arbitrary value of '0' as genesis block and using quotes to work with SHA-256
         self.create_block(proof = 1, previous_hash = '0')
+
+    # Method to create genesis block and next blocks once mined
+    # The create_block method is applied right after mining a block
+    def create_block(self, proof, previous_hash):
+        # Block stored in a dict with 4 keys: index, timestamp - exact time when block is mined,
+        # proof - found when block mined, and previous_hash
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()), # Use string to avoid formatting issues
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        # Append block to self.chain list
+        self.chain.append(block)
+        # Return block to display the 4 keys of this block in Postman
+        return block
