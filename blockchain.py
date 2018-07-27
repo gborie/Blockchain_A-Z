8b90ad2127ad5cdce8bf3bac1bc43a1f17f50bf2
gborie <gborie7@gmail.com>
2018-07-27 11:41:57 +0100

    Libraries added

---
+++ blockchain.py
@@ -0,0 +1,10 @@
+# Module 1 - Create a Blockchain
+
+# Each block will have its own timestamp
+import datetime
+# Hash the blocks
+import hashlib
+# Encode blocks before hashing them
+import json
+# Web application with Flask and return messages in Postman with jsonify
+from flask import Flask, jsonify
